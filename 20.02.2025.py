dca68a2770455623ec60d243f8935d0bfe7a7098
Ace_CoLd_HoOdY <max.gongalo@gmail.com>
2025-02-20 14:12:00 +0200

    Did a bit of tasks
    played blooket - 9400 dmg when everyone else 1500 - max

---
+++ 20.02.2025.py
@@ -0,0 +1,15 @@
+#20.02.2025
+
+
+#Natural numbers triangle
+n = 9
+for i in range (1, n+1):
+    for j in range(i):
+        print(i, end='') 
+    print()
+
+#Triangle from lots of numbers
+for i in range (1, 15):
+    for j in range (i):
+        print (j, end="")
+    print()
