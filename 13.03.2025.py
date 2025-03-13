6a3fbd874733c76a3f0f976d3c8b09f7ad685e6f
Ace_CoLd-HoOdY <max.gongalo@gmail.com>
2025-03-13 14:06:57 +0200

    Cycle for, numbers, words

---
+++ 13.03.2025.py
@@ -0,0 +1,27 @@
+# 16.01.2025
+
+
+#Number of vowels
+# word = input("Enter any word: ")
+# counter = 0
+# vowels = "eEuUiIoOaA"
+# for i in word:
+#     if i in vowels: 
+#         counter += 1
+#         print(i, end=" ")
+# print ()
+# print (counter)
+
+#Only even
+# numbers = []
+# for i in range(10):
+#     numbers.append(int(input('Enter numbers: ')))
+# for i in numbers:
+#     if i % 2 == 0:
+#         print (i, end=" ")
+
+#Squared numbers
+# numbers = []
+# for i in range(10):
+#     numbers.append((int(input('Enter numbers: '))) ** 2)
+# print(numbers)
