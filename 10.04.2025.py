2fb7b1d7b16e66602e25deaa013ace2a3686c2bb
Ace_CoLd_HoOdY <max.gongalo@gmail.com>
2025-04-10 14:10:24 +0300

    Def function

---
+++ 10.04.2025.py
@@ -0,0 +1,14 @@
+# # 10.04.2025
+
+
+#Triangle with def
+# def draw_triangle():
+#     for i in range(11):
+#         print("*" *i)
+# draw_triangle()
+
+#Rectangle with def
+def draw_rectangle(w, h):
+    for i in range(w):
+        print("*" *h)
+draw_rectangle(5, 6)
